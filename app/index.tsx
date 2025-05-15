bf1f80c9b6933eb0b3bb086b431836846b5e7f9b
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2025-05-15 16:39:50 -0500

    updated selection and deletion options

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FlatList,
   Image,
@@ -15,69 +16,96 @@ import Feather from "@expo/vector-icons/Feather";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import iconImage from "@/assets/images/user-icon.png";
-import { todoData } from "@/constants/list_data";
+import { todoData, ToDoType } from "@/constants/list_data";
 
 export default function Index() {
+  const [todos, setTodos] = useState<ToDoType[]>(todoData);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const ToDoItem = ({ todo }: { todo: ToDoType }) => (
+    <View style={styles.todoItem}>
+      <Text style={styles.todoStatus}>
+        {todo.isDone ? (
+          <Feather name="check-circle" size={24} color="black" />
+        ) : (
+          <Feather name="circle" size={24} color="black" />
+        )}
+      </Text>
+      <Text
+        style={[
+          styles.todoTitle,
+          todo.isDone && { textDecorationLine: "line-through" },
+        ]}
+      >
+        {todo.title}
+      </Text>
+      <TouchableOpacity
+        onPress={() => {
+          alert(`Delete ${todo.title}`);
+        }}
+      >
+        <Feather name="delete" size={24} color="black" />
+      </TouchableOpacity>
+      {/* <Ionicons name="pencil-sharp" size={24} color="black" /> */}
+    </View>
+  );
+
+  // const searchFunction = (query: string) => {
+  //   const filteredTodos = todos.filter(todo) =>todo.title.toLower
+  // };
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <TouchableOpacity
-            onPress={() => {
-              alert("clicked!");
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            alert("clicked!");
+          }}
+          activeOpacity={0} //default is .2 which reduces by 80%
+        >
+          <Ionicons name="menu" size={30} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={iconImage}
+            style={{
+              height: 50,
+              width: 50,
+              borderRadius: 25,
+              borderColor: "white",
+              borderWidth: 0,
             }}
-            activeOpacity={0} //default is .2 which reduces by 80%
-          >
-            <Ionicons name="menu" size={30} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              source={iconImage}
-              style={{
-                height: 50,
-                width: 50,
-                borderRadius: 25,
-                borderColor: "white",
-                borderWidth: 0,
-              }}
-            ></Image>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.searchBar}>
-          <Feather name="search" size={24} color="black" />
-          <TextInput
-            placeholder="search"
-            style={styles.searchInput}
-            clearButtonMode="always"
-          />
-        </View>
-        <FlatList
-          data={todoData}
-          renderItem={({ item }) => (
-            <View style={styles.todoItem}>
-              <Text style={styles.todoTitle}>{item.title}</Text>
-              <Text style={styles.todoStatus}>
-                {item.isDone ? "✅ Completed" : "🕓 Pending"}
-              </Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.id.toString()}
+          ></Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBar}>
+        <Feather name="search" size={24} color="black" />
+        <TextInput
+          placeholder="search"
+          style={styles.searchInput}
+          clearButtonMode="always"
         />
-        <View style={styles.footer}>
-          <TextInput
-            placeholder="Add to do item"
-            style={styles.newToDoInput}
-            clearButtonMode="always"
-          />
-          <TouchableOpacity onPress={() => {}} style={styles.addButton}>
-            <Ionicons name="add" size={32} color="black" />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </KeyboardAvoidingView>
+      </View>
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => <ToDoItem todo={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={10}
+        style={styles.footer}
+      >
+        <TextInput
+          placeholder="Add to do item"
+          style={styles.newToDoInput}
+          clearButtonMode="always"
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.addButton}>
+          <Ionicons name="add" size={32} color="black" />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
@@ -106,6 +134,8 @@ const styles = StyleSheet.create({
   },
   todoItem: {
     flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     backgroundColor: "white",
     padding: 16,
     marginVertical: 8,
@@ -140,5 +170,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 
-  addButton: { padding: 8, borderRadius: 10 },
+  addButton: {
+    backgroundColor: "#6200ee",
+    padding: 8,
+    borderRadius: 10,
+    marginLeft: 10,
+  },
 });
