c8cb49155a846d46d98d4ef47bb4209084401d2d
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2025-05-18 09:38:28 -0500

    updated splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+//setTimeout(SplashScreen.hideAsync, 1000);
 
 export default function RootLayout() {
+  useEffect(() => {
+    const prepare = async () => {
+      // await any async loading here (fonts, data)
+      await SplashScreen.hideAsync();
+    };
+    prepare();
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
